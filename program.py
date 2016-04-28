3094105237d98810456b0cea7fffa555b8654657
Luis Razo <lrazo@hotmail.com>
2016-04-29 01:00:40 +0200

    Adding data queries basic attempt

--- program.py
+++ program.py
@@ -1,5 +1,8 @@
 import csv
 import os
+import statistics
+
+from data_types import Purchase
 
 
 def main():
@@ -24,13 +27,13 @@ def get_data_file():
 def load_file(filename):
     with open(filename, 'r', encoding='utf8') as fin:
         reader = csv.DictReader(fin, delimiter=',')
+        purchases = []
         for row in reader:
-            print(row)
-            print("Bed count: {}".format(row['beds']))
-            # header = fin.readline().strip()
-            # reader = csv.reader(fin, delimiter=',' )
-            # for row in reader:
-            #     print(type(row), row)
+            #print(row)
+            #print("Bed count: {}".format(row['beds']))
+            p = Purchase.create_from_dict(row)
+            purchases.append(p)
+        return purchases
 
 
 def load_file_basic(filename):
@@ -46,8 +49,39 @@ def load_file_basic(filename):
         print(lines[:5])
 
 
-def query_data(data):
-    pass
+# def get_price(p):
+#    return p.price
+
+def query_data(data):  # list[Purchase]):
+
+    # if data was sorted by price:
+    # data.sort(key=get_price)
+    data.sort(key=lambda p: p.price)
+    # most expensive house
+    high_purchase = data[-1]
+    print("The most expensive house is ${:,} with {} beds and {} baths".format(high_purchase.price, high_purchase.beds,
+                                                                               high_purchase.baths))
+
+    # least expensive house
+    low_purchase = data[0]
+    print("The least expensive house is ${:,} with {} beds and {} baths".format(low_purchase.price, low_purchase.beds,
+                                                                                low_purchase.baths))
+    # average price house
+    prices = []
+    for pur in data:
+        prices.append(pur.price)
+
+    avg_price = statistics.mean(prices)
+    print("The average home price is ${:,}".format(int(avg_price)))
+
+    # average price house of 2 bedroom houses
+    prices = []
+    for pur in data:
+        if pur.beds == 2:
+            prices.append(pur.price)
+
+    avg_price = statistics.mean(prices)
+    print("The average of a 2-bedroom home price is ${:,}".format(int(avg_price)))
 
 
 if __name__ == '__main__':
